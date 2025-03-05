84c3e03c627fe267fb6412d5458eee1e3f240541
Mayanka11y <mayankraj418890@gmail.com>
2025-03-05 10:26:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your code here
+// script.js
+(function () {
+    'use strict';
+
+    document.addEventListener('DOMContentLoaded', function () {
+        var form = document.querySelector('.needs-validation');
+
+        form.addEventListener('submit', function (event) {
+            if (!form.checkValidity()) {
+                event.preventDefault();
+                event.stopPropagation();
+            }
+            form.classList.add('was-validated');
+        }, false);
+    });
+})();
