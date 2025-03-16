ec2b362904c46d3af92343a7c8a6bff63c17ccf6
Mayanka11y <mayankraj418890@gmail.com>
2025-03-16 12:35:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,3 @@
-//your code here
-// script.js
-(function () {
-    'use strict';
-
-    document.addEventListener('DOMContentLoaded', function () {
-        var form = document.querySelector('.needs-validation');
-
-        form.addEventListener('submit', function (event) {
-            if (!form.checkValidity()) {
-                event.preventDefault();
-                event.stopPropagation();
-            }// script.js
 (function () {
     'use strict';
 
@@ -27,10 +14,6 @@
             }
 
            
-            form.classList.add('was-validated');
-        }, false);
-    });
-})();
             form.classList.add('was-validated');
         }, false);
     });
