240ce37e3ebe9750c131eb504258ae82faafeeb1
Mayanka11y <mayankraj418890@gmail.com>
2025-03-16 12:31:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -10,7 +10,27 @@
             if (!form.checkValidity()) {
                 event.preventDefault();
                 event.stopPropagation();
+            }// script.js
+(function () {
+    'use strict';
+
+    document.addEventListener('DOMContentLoaded', function () {
+        
+        var form = document.getElementById('job-application-form');
+
+        
+        form.addEventListener('submit', function (event) {
+            
+            if (!form.checkValidity()) {
+                event.preventDefault(); 
+                event.stopPropagation(); 
             }
+
+           
+            form.classList.add('was-validated');
+        }, false);
+    });
+})();
             form.classList.add('was-validated');
         }, false);
     });
